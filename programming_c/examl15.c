1e016a24665d9a648b37741560ecaa669986b9f5
User44314 <take1234a@gmail.com>
2025-02-26 03:07:23 -0800

    coding 3

---
+++ programming_c/examl15.c
@@ -0,0 +1,82 @@
+/******************************************************
+  Write a program to check the syntax of a C program
+  for matching {} () "" '' [] pairs. Don't forget about
+  comments.
+******************************************************/
+#include <stdio.h>
+
+#define MAXLINE 1000 /* max input line size */
+char line[MAXLINE];  /*current input line*/
+
+int getline(void); /* read a line into line, return length */
+
+int main()
+{
+  int len = 0;
+  int t = 0;
+  int brace = 0, bracket = 0, parenthesis = 0;
+  int s_quote = 1, d_quote = 1;
+
+  while ((len = getline()) > 0)
+  {
+    t = 0;
+    while (t < len)
+    {
+      if (line[t] == '[')
+      {
+        brace++; // increment the brace count
+      }
+      if (line[t] == ']')
+      {
+        brace--; // decrement the brace count
+      }
+      if (line[t] == '(')
+      {
+        parenthesis++;
+      }
+      if (line[t] == ')')
+      {
+        parenthesis--;
+      }
+      if (line[t] == '\'')
+      {
+        s_quote *= -1; // toggle the single quote mark
+      }
+      if (line[t] == '"')
+      {
+        d_quote *= -1;
+      }
+      t++;
+    }
+  }
+  if (d_quote != 1)
+    printf("Mismatching double quote mark\n");
+  if (s_quote != 1)
+    printf("Mismatching single quote mark\n");
+  if (parenthesis != 0)
+    printf("Mismatching parenthesis\n");
+  if (brace != 0)
+    printf("Mismatching brace mark\n");
+  if (bracket != 0)
+    printf("Mismatching bracket mark\n");
+  if (bracket == 0 && brace == 0 && parenthesis == 0 && s_quote == 1 && d_quote == 1)
+    printf("Syntax appears to be correct.\n");
+  return 0;
+}
+
+/* getline: specialized version */
+int getline(void)
+{
+  int c, i;
+  extern char line[];
+
+  for (i = 0; i < MAXLINE - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+    line[i] = c;
+  if (c == '\n')
+  {
+    line[i] = c;
+    ++i;
+  }
+  line[i] = '\0';
+  return i;
+}
